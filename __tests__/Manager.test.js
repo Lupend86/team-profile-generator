18f304120b778299251429ba06199b4805b007a1
James Scott <jamesdscottjr@gmail.com>
2022-02-20 19:27:35 -0500

    complete setup

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,15 @@
+// using Manager constructor 
+const Manager = require('../lib/Manager');
+
+// creating manager object  
+test('creates an Manager object', () => {
+    const manager = new Manager('James', 90, 'jamesdscottjr@gmail', 4);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const manager = new Manager('James', 90, 'jamesdscottjr@gmail.com');
+    expect(manager.getRole()).toEqual("Manager");
+}); 
